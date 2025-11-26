a854ac5fca36b8ef41399c021dd638027a9dfa90
Satheesh_Asokan <satzwebio@gmail.com>
2025-11-26 17:46:33 -0600

    test

--- feast/feast_repo/crypto_features/feature_repo/crypto_features.py
+++ feast/feast_repo/crypto_features/feature_repo/crypto_features.py
@@ -1,12 +1,13 @@
 from feast import FileSource, Entity, Field, FeatureView
-from feast.types import Float64, String
+from feast.types import Float64
 from feast.data_format import ParquetFormat
 
 crypto_source = FileSource(
     name="crypto_source",
-    path="s3://feast-feature-store/raw/*/*.parquet",
+    path="s3://feast-feature-store/raw/",
     timestamp_field="event_timestamp",
     file_format=ParquetFormat(),
+    s3_endpoint_override="http://minio-service.infra.svc.cluster.local:9000",
 )
 
 crypto_entity = Entity(name="symbol", join_keys=["symbol"])
